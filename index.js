8ff2934c0ae2a61dc22efafb6586f0b8dcaf1a52
jinia hossen <jiniahossen31@gmail.com>
2023-12-12 19:43:12 +0600

    carted item get delete update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const app=express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
 require('dotenv').config();
-const port=process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 
 
@@ -15,47 +16,91 @@ app.use(express.json())
 //database
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khygiij.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    //collections
+        //collections
 
-    const itemCollection=client.db('myDB').collection('shoes');
-    const categoryCollection=client.db('myDB').collection('category');
-
-
-
-    //get products data
-
-    app.get('/shoes',async(req,res)=>{
-        const result=await itemCollection.find().toArray();
-        res.send(result)
-    })
-
-
-    //get category data
-    app.get('/category',async(req,res)=>{
-        const result=await categoryCollection.find().toArray();
-        res.send(result);
-    })
+        const itemCollection = client.db('myDB').collection('shoes');
+        const categoryCollection = client.db('myDB').collection('category');
+        const userCollection = client.db('myDB').collection('users');
+        const cartCollection = client.db('myDB').collection('cart');
 
 
 
+        //get products data
+
+        app.get('/shoes', async (req, res) => {
+            const result = await itemCollection.find().toArray();
+            res.send(result)
+        })
+
+
+        //get category data
+        app.get('/category', async (req, res) => {
+            const result = await categoryCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        //post user
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+        //my cart
+        app.post('/cart', async (req, res) => {
+            const cart = req.body;
+            const result = await cartCollection.insertOne(cart);
+            res.send(result);
+        })
+
+        //get my cart
+        app.get('/cart', async (req, res) => {
+            const cart = await cartCollection.find().toArray();
+            res.send(cart);
+        });
+
+        //get my carted item by email
+        app.get('/cart/:email', async (req, res) => {
+            const userEmail = req.params.email;
+            const cart = await cartCollection.find({ email: userEmail }).toArray();
+            res.send(cart);
+        });
+
+
+        //delet from my cart 
+
+        app.delete('/cart/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) }
+                const result = await cartCollection.deleteOne(query);
+                console.log(result);  // Log the result to see if it's successful
+                res.send({ success: true, message: 'Item deleted successfully' });
+            } catch (error) {
+                console.error('Error deleting item:', error);
+                res.status(500).send({ success: false, message: 'Failed to delete item' });
+            }
+        });
 
 
 
@@ -73,23 +118,26 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
     res.send('Nike store server is running')
 })
 
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
     console.log(`server running at port:${port}`);
 })
